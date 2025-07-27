429a23ef02778b7de39c989ebeb4e91de7b60db7
Profilist <84641209+Profilist@users.noreply.github.com>
2025-07-27 18:14:11 -0400

    Removes cutoff border on mobile for search bar

--- client/src/components/AnimatedSearchBar.tsx
+++ client/src/components/AnimatedSearchBar.tsx
@@ -116,37 +116,37 @@ export default function AnimatedSearchBar({
           whileTap={{ scale: 0.98 }}
           transition={{ type: "spring", stiffness: 400, damping: 17 }}
         >
-          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[70px] max-w-[314px] rounded-xl blur-[3px] 
+          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[70px] max-w-[314px] rounded-xl blur-[3px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-within:opacity-100 
                           before:absolute before:content-[''] before:z-[-2] before:w-[999px] before:h-[999px] before:bg-no-repeat before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rotate-60
                           before:bg-[conic-gradient(#0d0d0d,#7a2246_5%,#0d0d0d_38%,#0d0d0d_50%,#f2858e_60%,#0d0d0d_87%)] before:transition-all before:duration-2000
-                          group-hover:before:rotate-[-120deg] group-focus-within:before:rotate-[420deg] group-focus-within:before:duration-[4000ms]">
+                          group-hover:before:rotate-[-120deg] group-focus-within:before:animate-[spin_4s_linear_infinite]">
           </div>
-          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[65px] max-w-[312px] rounded-xl blur-[3px] 
+          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[65px] max-w-[312px] rounded-xl blur-[3px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-within:opacity-100 
                           before:absolute before:content-[''] before:z-[-2] before:w-[600px] before:h-[600px] before:bg-no-repeat before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rotate-[82deg]
                           before:bg-[conic-gradient(rgba(0,0,0,0),#5a1a35,rgba(0,0,0,0)_10%,rgba(0,0,0,0)_50%,#f2858e,rgba(0,0,0,0)_60%)] before:transition-all before:duration-2000
-                          group-hover:before:rotate-[-98deg] group-focus-within:before:rotate-[442deg] group-focus-within:before:duration-[4000ms]">
+                          group-hover:before:rotate-[-98deg] group-focus-within:before:animate-[spin_4s_linear_infinite]">
           </div>
-          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[65px] max-w-[312px] rounded-xl blur-[3px] 
+          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[65px] max-w-[312px] rounded-xl blur-[3px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-within:opacity-100 
                           before:absolute before:content-[''] before:z-[-2] before:w-[600px] before:h-[600px] before:bg-no-repeat before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rotate-[82deg]
                           before:bg-[conic-gradient(rgba(0,0,0,0),#5a1a35,rgba(0,0,0,0)_10%,rgba(0,0,0,0)_50%,#f2858e,rgba(0,0,0,0)_60%)] before:transition-all before:duration-2000
-                          group-hover:before:rotate-[-98deg] group-focus-within:before:rotate-[442deg] group-focus-within:before:duration-[4000ms]">
+                          group-hover:before:rotate-[-98deg] group-focus-within:before:animate-[spin_4s_linear_infinite]">
           </div>
-          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[65px] max-w-[312px] rounded-xl blur-[3px] 
+          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[65px] max-w-[312px] rounded-xl blur-[3px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-within:opacity-100 
                           before:absolute before:content-[''] before:z-[-2] before:w-[600px] before:h-[600px] before:bg-no-repeat before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rotate-[82deg]
                           before:bg-[conic-gradient(rgba(0,0,0,0),#5a1a35,rgba(0,0,0,0)_10%,rgba(0,0,0,0)_50%,#f2858e,rgba(0,0,0,0)_60%)] before:transition-all before:duration-2000
-                          group-hover:before:rotate-[-98deg] group-focus-within:before:rotate-[442deg] group-focus-within:before:duration-[4000ms]">
+                          group-hover:before:rotate-[-98deg] group-focus-within:before:animate-[spin_4s_linear_infinite]">
           </div>
 
-          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[63px] max-w-[307px] rounded-lg blur-[2px] 
+          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[63px] max-w-[307px] rounded-lg blur-[2px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-within:opacity-100 
                           before:absolute before:content-[''] before:z-[-2] before:w-[600px] before:h-[600px] before:bg-no-repeat before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rotate-[83deg]
                           before:bg-[conic-gradient(rgba(0,0,0,0)_0%,#7a2246,rgba(0,0,0,0)_8%,rgba(0,0,0,0)_50%,#f2858e,rgba(0,0,0,0)_58%)] before:brightness-140
-                          before:transition-all before:duration-2000 group-hover:before:rotate-[-97deg] group-focus-within:before:rotate-[443deg] group-focus-within:before:duration-[4000ms]">
+                          before:transition-all before:duration-2000 group-hover:before:rotate-[-97deg] group-focus-within:before:animate-[spin_4s_linear_infinite]">
           </div>
 
-          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[59px] max-w-[303px] rounded-xl blur-[0.5px] 
+          <div className="absolute z-[-1] overflow-hidden h-full w-full max-h-[59px] max-w-[303px] rounded-xl blur-[0.5px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-within:opacity-100 
                           before:absolute before:content-[''] before:z-[-2] before:w-[600px] before:h-[600px] before:bg-no-repeat before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rotate-70
                           before:bg-[conic-gradient(#1a1a1a,#7a2246_5%,#1a1a1a_14%,#1a1a1a_50%,#f2858e_60%,#1a1a1a_64%)] before:brightness-130
-                          before:transition-all before:duration-2000 group-hover:before:rotate-[-110deg] group-focus-within:before:rotate-[430deg] group-focus-within:before:duration-[4000ms]">
+                          before:transition-all before:duration-2000 group-hover:before:rotate-[-110deg] group-focus-within:before:animate-[spin_4s_linear_infinite]">
           </div>
 
           <div id="main" className="relative group">
